97a68d651ca70c79bb43b6a85d34978d2412152b
Mohamed Ben Chamakh <mohamed.ben.chamakh98@gmail.com>
2021-12-08 20:08:52 +0100

    Correction

--- Svm/main.py
+++ Svm/main.py
@@ -9,9 +9,9 @@ def SVM_service():
     if request.method == 'POST':
         json_data = request.get_json(force=True) 
         wav_music=json_data['wav_music']
-        data=svm()
+        
         genre=predict(wav_music)
-        return jsonify({"genre":genre,"svm":data})
+        return jsonify({"genre":genre})
 
 if __name__ == '__main__':
     app.run(debug = True)
--- Svm/svm.py
+++ Svm/svm.py
@@ -9,6 +9,7 @@ import librosa.feature
 
 import os # C'est ce qui va nous permettre d'itérer sur les fichiers de l'environnement de travail
 
+from time import sleep
 import joblib
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.model_selection import train_test_split, validation_curve, RandomizedSearchCV # Split de dataset et optimisation des hyperparamètres
@@ -45,13 +46,15 @@ def svm():
     data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size = 0.33)
     print('length of data_train:',len(data_train))
     print('length of data_test:',len(data_test))
-    model = SVC(kernel='rbf', C=10)
+    model = SVC(kernel='rbf', C=100)
     model.fit(data_train, labels_train)
-    modelname = 'model_svc.sav'
-    os.remove(modelname)
+    modelname = 'model_svm.sav'
+    #os.remove(modelname)
     pickle.dump(model, open(modelname, 'wb'))
-    #print('Train score : ', model.score(data_train,labels_train))
-    #print('Test score : ', model.score(data_test,labels_test))
+    print("Accuracy on training set: {:.3f}".format(model.score(data_train, labels_train)))
+    print("Accuracy on test set: {:.3f}".format(model.score(data_test, labels_test)))
+    print('Train score : ', model.score(data_train,labels_train))
+    print('Test score : ', model.score(data_test,labels_test))
     pred = model.predict(data_test)
     #print("Accuracy:",metrics.accuracy_score(labels_test, pred))
     #print(pred)
@@ -69,18 +72,21 @@ def grid_search(data_train,labels_train):
     print(grid_svm.best_params_)
 
 def predict(audio):
-    #audio="reggae.00000.wav"
+    df = pd.read_csv("../data/features_30_sec.csv")
+    df = df.drop(labels='filename', axis=1)
+    standardizer=StandardScaler()
+    data=standardizer.fit_transform(np.array(df.iloc[:,:-1],dtype=float))
     csv_file = csv.reader(open("../data/features_30_sec.csv", "r"), delimiter=",")
-    data=[]
+    i=0
     for row in csv_file:
         if audio == row[0]:
-            data=np.array([row[1:-1]])
-    data=data.astype(float)
+            data=np.array([data[i]],dtype=float)
+        i+=1
     if len(data) >0 :       
-        svm = joblib.load('model_svc.sav')
+        svm = joblib.load('model_svm.sav')
         print("----------------------------------- Predicted Labels -----------------------------------\n")
+        
         preds = svm.predict(data)
-        #print(preds)
         switcher = {
             0:"blues",
             1: "classical",
@@ -93,7 +99,7 @@ def predict(audio):
             8: "reggae",
             9: "rock",
         }
-        print(preds)
+        #print(preds)
         func = switcher.get(preds[0], lambda: "Invalid gender")
         print("Audio : ",audio)
         print("Genre: ",func)
@@ -101,14 +107,15 @@ def predict(audio):
         print("----------------------------------------------------------------------------------------")
         return func
 
-"""
-svm()
-#predict("rock.00007.wav")
+#svm()
+#predict("blues.00007.wav")
 
+"""
 csv_file = csv.reader(open("../data/features_30_sec.csv", "r"), delimiter=",")
 next(csv_file, None)
 
+
 for row in csv_file:
     predict(row[0])
-    time.sleep(0.5)
+    sleep(0.1)
 """
